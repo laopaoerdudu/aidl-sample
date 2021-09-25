638c5b1d5b6cfd96b1ddb507f0655e0182b93c78
David <18588744489@163.com>
2021-09-25 11:57:24 +0800

    Update

--- client/src/main/java/com/client/AIDLActivity.kt
+++ client/src/main/java/com/client/AIDLActivity.kt
@@ -100,6 +100,7 @@ class AIDLActivity : AppCompatActivity(), View.OnClickListener {
                 Log.i("WWE", "Client transfer { $book } to server by In and Out")
             }
             R.id.btnGetBooks -> {
+                tvDisplayBooks.text = ""
                 tvDisplayBooks.text = StringBuilder().apply {
                     bookManager.books.forEach { book ->
                         append(book.toString()).append("\n")
